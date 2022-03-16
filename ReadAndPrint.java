7b8f4e264149d72fbd2a5eab98e1d19ff20137dc
Don <dongeevarghese44@gmail.com>
2022-03-17 03:26:40 +0530

    added a pgm to read name and roll_no

--- ReadAndPrint.java
+++ ReadAndPrint.java
@@ -1,13 +1,14 @@
 import java.util.*;
-class Test{
- 
-    public static void main(String args[])
+class ReadAndPrint{
+     public static void main(String args[])
     {
         
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter Name");
         String k=sc.nextLine();
-        System.out.println("Your name  is:"+k);
+        System.out.println("Enter Roll Number");
+        int mn=sc.nextInt();
+        System.out.println("Your name  is:"+k+"\n Roll Number is:"+mn);
     }
 }
 /*Write a program to read name,roll_no,ad mark of a student and display details*/
