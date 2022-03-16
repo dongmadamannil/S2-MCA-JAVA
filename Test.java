65242405626bb6b59aba9d9f33ebc57d1fef7287
Don <dongeevarghese44@gmail.com>
2022-03-17 03:24:31 +0530

    added test file

---
+++ Test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Test{
+ 
+    public static void main(String args[])
+    {
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Name");
+        String k=sc.nextLine();
+        System.out.println("Your name  is:"+k);
+    }
+}
