40525005ce69b8c3612651dcebe88ce7c190da62
Don <dongeevarghese44@gmail.com>
2022-03-21 12:02:55 +0530

    java lab cycle co1

---
+++ Lab_Java/src/CO1/Qn2.java
@@ -0,0 +1,24 @@
+package CO1;
+import java.util.*;
+public class Qn2 {
+public static void main(String args[]){
+	System.out.println("Enter Row and Column Size");
+	Scanner obj=new Scanner(System.in);
+	int m=obj.nextInt();
+	int n=obj.nextInt();
+	ArGet a=new ArGet(m,n);
+	ArGet b=new ArGet(m,n);
+}
+}
+class ArGet{
+	int a[][]=new int[50][50];
+	ArGet(int m,int n){
+		
+		System.out.println("Enter "+m+"X"+n+" Elements");
+		Scanner obj=new Scanner(System.in);
+		for(int i=0;i<m;i++)
+			for(int j=0;j<n;++j)
+				a[i][j]=obj.nextInt();
+	}
+}
+
--- ReadAndPrint.java
+++ ReadAndPrint.java
@@ -3,11 +3,11 @@ class ReadAndPrint{
      public static void main(String args[])
     {
         
-        Scanner sc=new Scanner(System.in);
+        Scanner sic=new Scanner(System.in);
         System.out.println("Enter Name");
-        String k=sc.nextLine();
+        String k=sic.nextLine();
         System.out.println("Enter Roll Number");
-        int mn=sc.nextInt();
+        int mn=sic.nextInt();
         System.out.println("Your name  is:"+k+"\n Roll Number is:"+mn);
     }
 }
